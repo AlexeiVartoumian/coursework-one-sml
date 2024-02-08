4bd764879a58b9440ca2c55af1a1a54a7f36500b
Keith Mannock <k.mannock@bbk.ac.uk>
2024-02-08 09:01:31 +0000

    Refresh

---
+++ src/sml/Translator.java
@@ -0,0 +1,190 @@
+package sml;
+
+import sml.instructions.AddInstruction;
+import sml.instructions.LinInstruction;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * This class ....
+ * <p>
+ * The translator of a <code>SML</code> program.
+ *
+ * @author ...
+ */
+public final class Translator {
+
+    private static final String PATH = "";
+
+    // word + line is the part of the current line that's not yet processed
+    // word has no whitespace
+    // If word and line are not empty, line begins with whitespace
+    private final String fileName; // source file of SML code
+    private String line = "";
+
+    public Translator(final String file) {
+        fileName = PATH + file;
+    }
+
+    // translate the small program in the file into lab (the labels) and
+    // prog (the program)
+    // return "no errors were detected"
+
+    public boolean readAndTranslate(final Labels lab, final List<Instruction> prog) {
+        try (var sc = new Scanner(new File(fileName), StandardCharsets.UTF_8)) {
+            // Scanner attached to the file chosen by the user
+            // The labels of the program being translated
+            lab.reset();
+            // The program to be created
+            prog.clear();
+
+            try {
+                line = sc.nextLine();
+            } catch (NoSuchElementException ioE) {
+                return false;
+            }
+
+            // Each iteration processes line and reads the next input
+            // line into "line"
+            while (line != null) {
+                // Store the label in label
+                var label = scan();
+
+                if (label.length() > 0) {
+                    var ins = getInstruction(label);
+                    if (ins != null) {
+                        lab.addLabel(label);
+                        prog.add(ins);
+                    }
+                }
+
+                try {
+                    line = sc.nextLine();
+                } catch (NoSuchElementException ioE) {
+                    return false;
+                }
+            }
+        } catch (IOException ioE) {
+            System.err.println("File: IO error " + ioE);
+            return false;
+        }
+        return true;
+    }
+
+    // The input line should consist of an SML instruction, with its label already removed.
+    // Translate line into an instruction with label "label" and return the instruction.
+    public Instruction getInstruction(final String label) {
+        int s1; // Possible operands of the instruction
+        int s2;
+        int r;
+        String lbl;
+
+        if (line.isEmpty()) {
+            return null;
+        }
+        var opCode = scan();
+        
+        switch (opCode) {
+            case "add" -> {
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new AddInstruction(label, r, s1, s2);
+            }
+            case "lin" -> {
+                r = scanInt();
+                s1 = scanInt();
+                return new LinInstruction(label, r, s1);
+            }
+
+            // TODO: You will have to write code here for the other instructions.
+
+            default -> System.out.println(STR."Unknown instruction: \{opCode}");
+        }
+        return null; // FIX THIS
+    }
+
+    /*
+     * Return the first word of line and remove it from line. If there is no word,
+     * return ""
+     */
+    private String scan() {
+        line = line.trim();
+        if (line.isEmpty()) {
+            return "";
+        }
+
+        int i = 0;
+        while (i < line.length() && line.charAt(i) != ' ' && line.charAt(i) != '\t') {
+            i = i + 1;
+        }
+        String word = line.substring(0, i);
+        line = line.substring(i);
+        return word;
+    }
+
+    // Return the first word of line as an integer. If there is any error, return
+    // the maximum int
+    private int scanInt() {
+        String word = scan();
+        if (word.isEmpty()) {
+            return Integer.MAX_VALUE;
+        }
+
+        try {
+            return Integer.parseInt(word);
+        } catch (NumberFormatException e) {
+            return Integer.MAX_VALUE;
+        }
+    }
+
+    private Instruction returnInstruction(final String label, String opCode) {
+        String pkg = "sml.instructions";
+        String base = "Instruction";
+
+        // add -> sml.instructions.AddInstruction
+
+        String fqcn = pkg + "." + opCode.substring(0, 1).toUpperCase(Locale.ROOT) + opCode.substring(1) + base;
+
+        Class<?> clazz;
+        try {
+            clazz = Class.forName(fqcn);
+        } catch (ClassNotFoundException e) {
+            System.err.println(STR."Unknown instruction: \{fqcn}");
+            return null;
+        }
+
+        // find the correct constructor
+        var cons = findConstructor(clazz);
+        var objArray = argsForConstructor(null, label);
+
+        try {
+            return (Instruction) cons.newInstance(objArray);
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException | NullPointerException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    private Constructor<?> findConstructor(Class<?> cl) {
+        Constructor<?> cons = null;
+        // TODO
+        return null;
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    private Object[] argsForConstructor(Constructor<?> cons, String label) {
+        Object[] argsArray = null;
+        // TODO
+        return null;
+    }
+}
---
+++ src/sml/instructions/LinInstruction.java
@@ -0,0 +1,29 @@
+package sml.instructions;
+
+import sml.Instruction;
+import sml.Machine;
+
+public final class LinInstruction extends Instruction {
+    private String label;
+    private final int register;
+    private final int value;
+    private int register2;
+
+    public LinInstruction(final String label, int register, int value) {
+        super(label, "lin");
+        this.register = register;
+        this.value = value;
+    }
+
+    @Override
+    public void execute(Machine m) {
+
+        m.registers().register(register, value);
+        // TODO
+    }
+
+    @Override
+    public String toString() {
+        return STR."\{STR."\{super.toString()} store in register " + register} the value " + value;
+    }
+}
