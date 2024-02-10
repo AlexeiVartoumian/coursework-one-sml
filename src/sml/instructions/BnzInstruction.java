2904ab54e722ca0a435df8356e4093b1698ba599
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-02-10 11:54:28 +0000

    "Configuration Hell. Cant run Lombok and commandline args in terminal. No idea why. must pass in command-line args through edit config to solve that"

---
+++ src/sml/instructions/BnzInstruction.java
@@ -0,0 +1,8 @@
+package sml.instructions;
+
+/**
+ * Branch Instruction bnz: for --> label L1 Terminology below
+ * L1 bnz s1 L2	If the contents of register s1 is not zero, then make the statement labeled L2 the next statement to execute
+ */
+public class BnzInstruction {
+}
